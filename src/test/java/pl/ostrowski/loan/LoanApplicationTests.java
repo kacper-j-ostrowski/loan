d010583a8a5b594dfd08d32988ca5568f9a5ab5e
Kacper Ostrowski <kacper.ostrowski@roche.com>
2018-12-14 10:01:55 +0100

    init

---
+++ src/test/java/pl/ostrowski/loan/LoanApplicationTests.java
@@ -0,0 +1,17 @@
+package pl.ostrowski.loan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
