669642e9b1e255c63795b42292642266c434e59c
Kacper Ostrowski <kacper.ostrowski@roche.com>
2018-12-24 22:10:27 +0100

    refactor

--- src/main/java/pl/ostrowski/loan/controller/LoanController.java
+++ src/main/java/pl/ostrowski/loan/controller/LoanController.java
@@ -3,7 +3,6 @@ package pl.ostrowski.loan.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,19 +16,14 @@ import pl.ostrowski.loan.dto.LoanRejectedRepsoneDto;
 import pl.ostrowski.loan.service.LoanService;
 import pl.ostrowski.loan.validators.loanapplication.LoanValidator;
 
-import javax.servlet.http.HttpServletRequest;
-import java.time.LocalTime;
 import java.util.Optional;
 
 @RestController
 public class LoanController {
 
-    Logger logger = LoggerFactory.getLogger(LoanController.class);
+    private static final Logger logger = LoggerFactory.getLogger(LoanController.class);
 
     private LoanService loanService;
-
-    private static final LocalTime MIDNIGHT = LocalTime.MIDNIGHT;
-    private static final LocalTime SIX_AM = LocalTime.NOON.minusHours(6);
 
     @Autowired
     public LoanController(LoanService loanService) {
