00e6a58be6b74edf788fdf0536269e3a7041df96
Kacper Ostrowski <kacper.ostrowski@roche.com>
2018-12-28 15:27:05 +0100

    simple case of test

--- src/test/java/pl/ostrowski/loan/service/AnnualPercentageRateCalculatorServiceImplTest.java
+++ src/test/java/pl/ostrowski/loan/service/AnnualPercentageRateCalculatorServiceImplTest.java
@@ -1,19 +1,27 @@
 package pl.ostrowski.loan.service;
 
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import pl.ostrowski.loan.dto.LoanDto;
+
+import java.math.BigDecimal;
+
+import static org.junit.Assert.assertEquals;
 
 public class AnnualPercentageRateCalculatorServiceImplTest {
 
-
-    @Autowired
-    AnnualPercentageRateCalculatorServiceImpl annualPercentageRateCalculatorService;
-
+    private AnnualPercentageRateCalculatorServiceImpl annualPercentageRateCalculatorService = new AnnualPercentageRateCalculatorServiceImpl();
 
     @Test
     public void test_calculateAnnualPercentageRate_SimpleCase() {
 
+        LoanDto loan = LoanDto.builder()
+                .amount(BigDecimal.valueOf(500))
+                .amount(BigDecimal.valueOf(510))
+                .daysToRepayment(25)
+                .build();
+
+        BigDecimal result = annualPercentageRateCalculatorService.calculate(loan);
+
+        assertEquals(0, BigDecimal.valueOf(33.5).compareTo(result));
     }
-
-
 }
