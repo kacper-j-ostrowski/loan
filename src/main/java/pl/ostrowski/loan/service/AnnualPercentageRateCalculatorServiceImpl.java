062a601fdb1dccf3a6c9d5650548900dace8f67b
Kacper Ostrowski <kacper.ostrowski@roche.com>
2019-01-02 09:36:04 +0100

    annual percentage rate calculator simple implementation

--- src/main/java/pl/ostrowski/loan/service/AnnualPercentageRateCalculatorServiceImpl.java
+++ src/main/java/pl/ostrowski/loan/service/AnnualPercentageRateCalculatorServiceImpl.java
@@ -3,13 +3,16 @@ package pl.ostrowski.loan.service;
 import pl.ostrowski.loan.dto.LoanDto;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.chrono.IsoChronology;
 
 public class AnnualPercentageRateCalculatorServiceImpl implements AnnualPercentageRateCalculatorService {
 
     @Override
     public BigDecimal calculate(LoanDto loan) {
-         double apr = 100 * (Math.pow((loan.getDueAmount().divide(loan.getAmount())).doubleValue(),
-                 365/loan.getDaysToRepayment().doubleValue()) - 1);
+        int daysInYear = IsoChronology.INSTANCE.isLeapYear(LocalDate.now().getYear()) ? 366 : 365;
+        double apr = 100 * (Math.pow((loan.getDueAmount().divide(loan.getAmount())).doubleValue(),
+                daysInYear/loan.getDaysToRepayment().doubleValue()) - 1);
 
         return BigDecimal.valueOf(apr).setScale(1,BigDecimal.ROUND_HALF_UP);
     }
