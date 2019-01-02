5260de890d1519776284a4279110c040b86d3111
Kacper Ostrowski <kacper.ostrowski@roche.com>
2019-01-02 11:04:34 +0100

    exception handling

---
+++ src/main/java/pl/ostrowski/loan/exceptionhandling/ExceptionHandlerImpl.java
@@ -0,0 +1,25 @@
+package pl.ostrowski.loan.exceptionhandling;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.UUID;
+
+@Log4j2
+@RestControllerAdvice
+public class ExceptionHandlerImpl {
+
+    @RequestMapping(produces = "application/json")
+    @ExceptionHandler(Throwable.class)
+    public ResponseEntity<?> handleExceptions(Throwable ex) {
+        UUID exceptionUUID = UUID.randomUUID();
+        log.error( "Error occured with message: {}. Error unique id: {}", ex.getMessage(), exceptionUUID);
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ex.getMessage());
+    }
+}
